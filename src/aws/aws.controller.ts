df20ae4087f2efc0b827318a88ec8866fb572643
YoujeongChoi <yj91322@gmail.com>
2024-03-06 18:07:29 +0900

    불필요한 코드 삭제

--- src/aws/aws.controller.ts
+++ src/aws/aws.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AwsService } from './aws.service';
-import { CreateAwDto } from './dto/create-aw.dto';
-import { UpdateAwDto } from './dto/update-aw.dto';
 
 @Controller('aws')
 export class AwsController {
--- src/aws/dto/update-aw.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAwDto } from './create-aw.dto';
-
-export class UpdateAwDto extends PartialType(CreateAwDto) {}
